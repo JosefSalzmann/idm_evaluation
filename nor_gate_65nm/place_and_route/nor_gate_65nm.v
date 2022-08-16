928c87b1116440dedcab6f7353c6dc5740fe7c2d
JosefSalzmann <josefsalzmann@hotmail.com>
2022-08-16 12:04:41 +0200

    update

--- nor_gate_65nm/place_and_route/nor_gate_65nm.v
+++ nor_gate_65nm/place_and_route/nor_gate_65nm.v
@@ -2,6 +2,6 @@ module nor_gate_65nm (myinA1, myinA2, myout);
        input myinA1, myinA2;
        output myout;
 
-       NR2M0R NOR0 ( .A (myinA1), .B (myinA2), .Z (myout));
+       NR2M0N NOR0 ( .A (myinA1), .B (myinA2), .Z (myout));
 
 endmodule
