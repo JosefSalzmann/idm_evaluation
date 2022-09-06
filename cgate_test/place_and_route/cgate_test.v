1bd7602cefa49c08d2c27a6038c85c178aff5244
JosefSalzmann <josefsalzmann@hotmail.com>
2022-09-06 14:06:49 +0200

    add cgate circuit

---
+++ cgate_test/place_and_route/cgate_test.v
@@ -0,0 +1,11 @@
+module nor_funnel (Input_A, Input_B, Output);
+       input Input_A, Input_B;
+       output Output;
+
+       wire A, B, Z;       
+       BUF_X8 BUFFER_A ( .I (Input_A), .Z (A) );
+       BUF_X8 BUFFER_B ( .I (Input_B), .Z (B) );
+       NOR2_X1 CGATE ( .A1 (A), .A2 (B), .ZN (Z));
+       BUF_X8 BUFFER_B ( .I (Z), .Z (Output) );
+
+endmodule
